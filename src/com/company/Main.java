958543d4e7cc3a91ffee8c03717a7a32a7dca61d
unatalbarbara <unatalbarbara@gmail.com>
2020-04-10 03:29:04 +0200

    metodos simples

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,34 @@
 package com.company;
 
 public class Main {
+    static String treuEspais(String s) {
+        String cad = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ')
+                cad += s.charAt(i);
+        }
+        return cad;
+    }
+
+    static String treuAccents(String s) {
+        String accents = "ÀÈÌÒÙÁÉÍÓÚÄËÏÖÜÂÊÎÔÛ";
+        String sense = "AEIOUAEIOUAEIOUAEIOU";
+        String cad = "";
+        char c;
+        int p;
+        for (int n = 0; n < s.length(); n++) {
+            c = s.charAt(n);
+            p = accents.indexOf(c);
+            if ((p != -1)) {
+                c = sense.charAt(p);
+                cad += c; //Reemplazar
+            } else //Si no tiene acentos, sumarla a la cadena
+                cad += c;
+        }
+        return cad;
+    }
 
     public static void main(String[] args) {
-
+//        palsearch = treuAccents(treuEspais(pal.toUpperCase()));
     }
 }
